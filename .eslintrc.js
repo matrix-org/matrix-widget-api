c767d503b89fa09a900895de98b35948680b9082
Travis Ralston <travpc@gmail.com>
2020-09-04 15:17:17 -0600

    Project template

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    extends: ["matrix-org"],
+    plugins: [
+        "babel",
+    ],
+    env: {
+        browser: true,
+    },
+    rules: {
+        "no-var": ["warn"],
+        "prefer-rest-params": ["warn"],
+        "prefer-spread": ["warn"],
+        "one-var": ["warn"],
+        "padded-blocks": ["warn"],
+        "no-extend-native": ["warn"],
+        "camelcase": ["warn"],
+        "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
+        "space-before-function-paren": ["error", {
+            "anonymous": "never",
+            "named": "never",
+            "asyncArrow": "always",
+        }],
+        "arrow-parens": "off",
+        "prefer-promise-reject-errors": "off",
+        "quotes": "off",
+        "indent": "off",
+        "no-constant-condition": "off",
+        "no-async-promise-executor": "off",
+    },
+    overrides: [{
+        "files": ["src/**/*.ts"],
+        "extends": ["matrix-org/ts"],
+        "rules": {
+            "quotes": "off",
+        },
+    }],
+};
