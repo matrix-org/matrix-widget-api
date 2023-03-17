matrix-org/matrix-widget-api

commit a8fc2b929564fac88d0bf0e6f175c8299190871e

Enable Typescript strict mode (#80)

src/ClientWidgetApi.ts
@@ -158,7 +158,7 @@ export class ClientWidgetApi extends EventEmitter {
             || this.hasCapability(`org.matrix.msc2762.timeline:${roomId}`);
     }
 
-    public canSendRoomEvent(eventType: string, msgtype: string = null): boolean {
+    public canSendRoomEvent(eventType: string, msgtype: string | null = null): boolean {
         return this.allowedEvents.some(e => e.matchesAsRoomEvent(EventDirection.Send, eventType, msgtype));
     }
 
@@ -170,7 +170,7 @@ export class ClientWidgetApi extends EventEmitter {
         return this.allowedEvents.some(e => e.matchesAsToDeviceEvent(EventDirection.Send, eventType));
     }
 
-    public canReceiveRoomEvent(eventType: string, msgtype: string = null): boolean {
+    public canReceiveRoomEvent(eventType: string, msgtype: string | null = null): boolean {
         return this.allowedEvents.some(e => e.matchesAsRoomEvent(EventDirection.Receive, eventType, msgtype));
     }
 
@@ -288,7 +288,7 @@ export class ClientWidgetApi extends EventEmitter {
             });
         }
 
-        const onErr = (e) => {
+        const onErr = (e: any) => {
             console.error("[ClientWidgetApi] Failed to handle navigation: ", e);
             return this.transport.reply<IWidgetApiErrorResponseData>(request, {
                 error: {message: "Error handling navigation"},
@@ -355,7 +355,7 @@ export class ClientWidgetApi extends EventEmitter {
                 return replyError("client provided invalid OIDC token for an allowed request");
             }
             if (update.state === OpenIDRequestState.Blocked) {
-                update.token = null; // just in case the client did something weird
+                update.token = undefined; // just in case the client did something weird
             }
 
             observer.close();
@@ -377,7 +377,7 @@ export class ClientWidgetApi extends EventEmitter {
             });
         }
 
-        let askRoomIds: string[] = null; // null denotes current room only
+        let askRoomIds: string[] | null = null; // null denotes current room only
         if (request.data.room_ids) {
             askRoomIds = request.data.room_ids as string[];
             if (!Array.isArray(askRoomIds)) {
@@ -397,7 +397,7 @@ export class ClientWidgetApi extends EventEmitter {
         let events: Promise<IRoomEvent[]> = Promise.resolve([]);
         if (request.data.state_key !== undefined) {
             const stateKey = request.data.state_key === true ? undefined : request.data.state_key.toString();
-            if (!this.canReceiveStateEvent(request.data.type, stateKey)) {
+            if (!stateKey || !this.canReceiveStateEvent(request.data.type, stateKey)) {
                 return this.transport.reply<IWidgetApiErrorResponseData>(request, {
                     error: {message: "Cannot read state events of this type"},
                 });
@@ -431,7 +431,7 @@ export class ClientWidgetApi extends EventEmitter {
         const isState = request.data.state_key !== null && request.data.state_key !== undefined;
         let sendEventPromise: Promise<ISendEventDetails>;
         if (isState) {
-            if (!this.canSendStateEvent(request.data.type, request.data.state_key)) {
+            if (!this.canSendStateEvent(request.data.type, request.data.state_key!)) {
                 return this.transport.reply<IWidgetApiErrorResponseData>(request, {
                     error: {message: "Cannot send state events of this type"},
                 });
@@ -444,7 +444,7 @@ export class ClientWidgetApi extends EventEmitter {
                 request.data.room_id,
             );
         } else {
-            const content = request.data.content || {};
+            const content = request.data.content as { msgtype?: string } || {};
             const msgtype = content['msgtype'];
             if (!this.canSendRoomEvent(request.data.type, msgtype)) {
                 return this.transport.reply<IWidgetApiErrorResponseData>(request, {
@@ -599,7 +599,7 @@ export class ClientWidgetApi extends EventEmitter {
                 if (e.state_key !== undefined) {
                     return this.canReceiveStateEvent(e.type, e.state_key);
                 } else {
-                    return this.canReceiveRoomEvent(e.type, e.content['msgtype']);
+                    return this.canReceiveRoomEvent(e.type, (e.content as { msgtype?: string })['msgtype']);
                 }
             });
 
@@ -717,7 +717,7 @@ export class ClientWidgetApi extends EventEmitter {
             }
         } else {
             // message event
-            if (!this.canReceiveRoomEvent(rawEvent.type, rawEvent.content?.["msgtype"])) {
+            if (!this.canReceiveRoomEvent(rawEvent.type, (rawEvent.content as { msgtype?: string })?.["msgtype"])) {
                 return; // no-op
             }
         }

src/models/WidgetEventCapability.ts
@@ -57,7 +57,7 @@ export class WidgetEventCapability {
         return true;
     }
 
-    public matchesAsRoomEvent(direction: EventDirection, eventType: string, msgtype: string = null): boolean {
+    public matchesAsRoomEvent(direction: EventDirection, eventType: string, msgtype: string | null = null): boolean {
         if (this.kind !== EventKind.Event) return false; // not a room event
         if (this.direction !== direction) return false; // direction mismatch
         if (this.eventType !== eventType) return false; // event type mismatch
@@ -124,9 +124,9 @@ export class WidgetEventCapability {
     public static findEventCapabilities(capabilities: Iterable<Capability>): WidgetEventCapability[] {
         const parsed: WidgetEventCapability[] = [];
         for (const cap of capabilities) {
-            let direction: EventDirection = null;
-            let eventSegment: string;
-            let kind: EventKind = null;
+            let direction: EventDirection | null = null;
+            let eventSegment: string | undefined;
+            let kind: EventKind | null = null;
 
             // TODO: Enable support for m.* namespace once the MSCs land.
             // https://github.com/matrix-org/matrix-widget-api/issues/22
@@ -158,14 +158,14 @@ export class WidgetEventCapability {
                 eventSegment = cap.substring("org.matrix.msc3819.receive.to_device:".length);
             }
 
-            if (direction === null || kind === null) continue;
+            if (direction === null || kind === null || eventSegment === undefined) continue;
 
             // The capability uses `#` as a separator between event type and state key/msgtype,
             // so we split on that. However, a # is also valid in either one of those so we
             // join accordingly.
             // Eg: `m.room.message##m.text` is "m.room.message" event with msgtype "#m.text".
             const expectingKeyStr = eventSegment.startsWith("m.room.message#") || kind === EventKind.State;
-            let keyStr: string = null;
+            let keyStr: string | null = null;
             if (eventSegment.includes('#') && expectingKeyStr) {
                 // Dev note: regex is difficult to write, so instead the rules are manually written
                 // out. This is probably just as understandable as a boring regex though, so win-win?

src/WidgetApi.ts
@@ -92,8 +92,8 @@ export class WidgetApi extends EventEmitter {
     private capabilitiesFinished = false;
     private supportsMSC2974Renegotiate = false;
     private requestedCapabilities: Capability[] = [];
-    private approvedCapabilities: Capability[];
-    private cachedClientVersions: ApiVersion[];
+    private approvedCapabilities?: Capability[];
+    private cachedClientVersions?: ApiVersion[];
     private turnServerWatchers = 0;
 
     /**
@@ -102,7 +102,7 @@ export class WidgetApi extends EventEmitter {
      * the API will use the widget ID from the first valid request it receives.
      * @param {string} clientOrigin The origin of the client, or null if not known.
      */
-    public constructor(widgetId: string = null, private clientOrigin: string = null) {
+    public constructor(widgetId: string | null = null, private clientOrigin: string | null = null) {
         super();
         if (!window.parent) {
             throw new Error("No parent window. This widget doesn't appear to be embedded properly.");

src/driver/WidgetDriver.ts
@@ -75,8 +75,8 @@ export abstract class WidgetDriver {
     public sendEvent(
         eventType: string,
         content: unknown,
-        stateKey: string = null,
-        roomId: string = null,
+        stateKey: string | null = null,
+        roomId: string | null = null,
     ): Promise<ISendEventDetails> {
         return Promise.reject(new Error("Failed to override function"));
     }
@@ -117,7 +117,7 @@ export abstract class WidgetDriver {
         eventType: string,
         msgtype: string | undefined,
         limit: number,
-        roomIds: string[] = null,
+        roomIds: string[] | null = null,
     ): Promise<IRoomEvent[]> {
         return Promise.resolve([]);
     }
@@ -141,7 +141,7 @@ export abstract class WidgetDriver {
         eventType: string,
         stateKey: string | undefined,
         limit: number,
-        roomIds: string[] = null,
+        roomIds: string[] | null = null,
     ): Promise<IRoomEvent[]> {
         return Promise.resolve([]);
     }

src/interfaces/IWidgetApiRequest.ts
@@ -31,4 +31,7 @@ export interface IWidgetApiRequest {
     action: WidgetApiAction;
     widgetId: string;
     data: IWidgetApiRequestData;
+    // XXX: This is for Scalar support
+    // TODO: Fix scalar
+    visible?: any;
 }

src/models/WidgetParser.ts
@@ -116,7 +116,7 @@ export class WidgetParser {
         // is done against the requirements of the interface because not everyone
         // will have an interface to validate against.
 
-        const content = stateEvent.content || {};
+        const content = stateEvent.content as IWidget || {};
 
         // Form our best approximation of a widget with the information we have
         const estimatedWidget: IWidget = {

src/templating/url-template.ts
@@ -46,7 +46,7 @@ export function runTemplate(url: string, widget: IWidget, params: ITemplateParam
         const pattern = `$${key}`.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
         const rexp = new RegExp(pattern, 'g');
 
-        // This is technically not what we're supposed to do for a couple reasons:
+        // This is technically not what we're supposed to do for a couple of reasons:
         // 1. We are assuming that there won't later be a $key match after we replace a variable.
         // 2. We are assuming that the variable is in a place where it can be escaped (eg: path or query string).
         result = result.replace(rexp, encodeURIComponent(toString(variables[key])));
@@ -58,5 +58,5 @@ export function toString(a: unknown): string {
     if (a === null || a === undefined) {
         return `${a}`;
     }
-    return a.toString();
+    return String(a);
 }

src/transport/ITransport.ts
@@ -38,7 +38,7 @@ export interface ITransport extends EventEmitter {
     /**
      * The widget ID, if known. If not known, null.
      */
-    readonly widgetId: string;
+    readonly widgetId: string | null;
 
     /**
      * If true, the transport will refuse requests from origins other than the
@@ -51,7 +51,7 @@ export interface ITransport extends EventEmitter {
      * The origin the transport should be replying/sending to. If not known, leave
      * null.
      */
-    targetOrigin: string;
+    targetOrigin: string | null;
 
     /**
      * The number of seconds an outbound request is allowed to take before it
@@ -62,12 +62,12 @@ export interface ITransport extends EventEmitter {
     /**
      * Starts the transport for listening
      */
-    start();
+    start(): void;
 
     /**
      * Stops the transport. It cannot be re-started.
      */
-    stop();
+    stop(): void;
 
     /**
      * Sends a request to the remote end.
@@ -100,5 +100,5 @@ export interface ITransport extends EventEmitter {
      * @param {IWidgetApiRequest} request The request to reply to.
      * @param {IWidgetApiResponseData} responseData The response data to reply with.
      */
-    reply<T extends IWidgetApiResponseData>(request: IWidgetApiRequest, responseData: T);
+    reply<T extends IWidgetApiResponseData>(request: IWidgetApiRequest, responseData: T): void;
 }

commit c2068f799c19ae29b203d512657de15328a933df

Fix reading state events without a specific state key
And add some tests for reading state events while we're at it.

Regressed by a8fc2b929564fac88d0bf0e6f175c8299190871e

src/ClientWidgetApi.ts
@@ -178,7 +178,7 @@ export class ClientWidgetApi extends EventEmitter {
         return this.allowedEvents.some(e => e.matchesAsRoomEvent(EventDirection.Receive, eventType, msgtype));
     }
 
-    public canReceiveStateEvent(eventType: string, stateKey: string): boolean {
+    public canReceiveStateEvent(eventType: string, stateKey: string | null): boolean {
         return this.allowedEvents.some(e => e.matchesAsStateEvent(EventDirection.Receive, eventType, stateKey));
     }
 
@@ -401,7 +401,7 @@ export class ClientWidgetApi extends EventEmitter {
         let events: Promise<IRoomEvent[]> = Promise.resolve([]);
         if (request.data.state_key !== undefined) {
             const stateKey = request.data.state_key === true ? undefined : request.data.state_key.toString();
-            if (!stateKey || !this.canReceiveStateEvent(request.data.type, stateKey)) {
+            if (!this.canReceiveStateEvent(request.data.type, stateKey ?? null)) {
                 return this.transport.reply<IWidgetApiErrorResponseData>(request, {
                     error: {message: "Cannot read state events of this type"},
                 });

src/models/WidgetEventCapability.ts
@@ -37,7 +37,7 @@ export class WidgetEventCapability {
     ) {
     }
 
-    public matchesAsStateEvent(direction: EventDirection, eventType: string, stateKey: string): boolean {
+    public matchesAsStateEvent(direction: EventDirection, eventType: string, stateKey: string | null): boolean {
         if (this.kind !== EventKind.State) return false; // not a state event
         if (this.direction !== direction) return false; // direction mismatch
         if (this.eventType !== eventType) return false; // event type mismatch

test/ClientWidgetApi-test.ts
@@ -28,6 +28,7 @@ import { WidgetApiFromWidgetAction } from '../src/interfaces/WidgetApiAction';
 import { WidgetApiDirection } from '../src/interfaces/WidgetApiDirection';
 import { Widget } from '../src/models/Widget';
 import { PostmessageTransport } from '../src/transport/PostmessageTransport';
+import { IReadEventFromWidgetActionRequest } from '../src';
 
 jest.mock('../src/transport/PostmessageTransport')
 
@@ -74,6 +75,7 @@ describe('ClientWidgetApi', () => {
         document.body.appendChild(iframe);
 
         driver = {
+            readStateEvents: jest.fn(),
             readEventRelations: jest.fn(),
             validateCapabilities: jest.fn(),
             searchUserDirectory: jest.fn(),
@@ -110,6 +112,127 @@ describe('ClientWidgetApi', () => {
         expect(clientWidgetApi.hasCapability('m.always_on_screen')).toBe(true);
         expect(clientWidgetApi.hasCapability('m.sticker')).toBe(false);
     });
+
+    describe('org.matrix.msc2876.read_events action', () => {
+        it('reads state events with any state key', async () => {
+            driver.readStateEvents.mockResolvedValue([
+                createRoomEvent({ type: 'net.example.test', state_key: 'A' }),
+                createRoomEvent({ type: 'net.example.test', state_key: 'B' }),
+            ])
+
+            const event: IReadEventFromWidgetActionRequest = {
+                api: WidgetApiDirection.FromWidget,
+                widgetId: 'test',
+                requestId: '0',
+                action: WidgetApiFromWidgetAction.MSC2876ReadEvents,
+                data: {
+                    type: 'net.example.test',
+                    state_key: true,
+                },
+            };
+
+            await loadIframe(['org.matrix.msc2762.receive.state_event:net.example.test']);
+
+            emitEvent(new CustomEvent('', { detail: event }));
+
+            await waitFor(() => {
+                expect(transport.reply).toBeCalledWith(event, {
+                    events: [
+                        createRoomEvent({ type: 'net.example.test', state_key: 'A' }),
+                        createRoomEvent({ type: 'net.example.test', state_key: 'B' }),
+                    ],
+                });
+            });
+
+            expect(driver.readStateEvents).toBeCalledWith(
+                'net.example.test', undefined, 0, null,
+            )
+        });
+
+        it('fails to read state events with any state key', async () => {
+            const event: IReadEventFromWidgetActionRequest = {
+                api: WidgetApiDirection.FromWidget,
+                widgetId: 'test',
+                requestId: '0',
+                action: WidgetApiFromWidgetAction.MSC2876ReadEvents,
+                data: {
+                    type: 'net.example.test',
+                    state_key: true,
+                },
+            };
+
+            await loadIframe([]); // Without the required capability
+
+            emitEvent(new CustomEvent('', { detail: event }));
+
+            await waitFor(() => {
+                expect(transport.reply).toBeCalledWith(event, {
+                    error: { message: expect.any(String) },
+                });
+            });
+
+            expect(driver.readStateEvents).not.toBeCalled()
+        });
+
+        it('reads state events with a specific state key', async () => {
+            driver.readStateEvents.mockResolvedValue([
+                createRoomEvent({ type: 'net.example.test', state_key: 'B' }),
+            ])
+
+            const event: IReadEventFromWidgetActionRequest = {
+                api: WidgetApiDirection.FromWidget,
+                widgetId: 'test',
+                requestId: '0',
+                action: WidgetApiFromWidgetAction.MSC2876ReadEvents,
+                data: {
+                    type: 'net.example.test',
+                    state_key: 'B',
+                },
+            };
+
+            await loadIframe(['org.matrix.msc2762.receive.state_event:net.example.test#B']);
+
+            emitEvent(new CustomEvent('', { detail: event }));
+
+            await waitFor(() => {
+                expect(transport.reply).toBeCalledWith(event, {
+                    events: [
+                        createRoomEvent({ type: 'net.example.test', state_key: 'B' }),
+                    ],
+                });
+            });
+
+            expect(driver.readStateEvents).toBeCalledWith(
+                'net.example.test', 'B', 0, null,
+            )
+        });
+
+        it('fails to read state events with a specific state key', async () => {
+            const event: IReadEventFromWidgetActionRequest = {
+                api: WidgetApiDirection.FromWidget,
+                widgetId: 'test',
+                requestId: '0',
+                action: WidgetApiFromWidgetAction.MSC2876ReadEvents,
+                data: {
+                    type: 'net.example.test',
+                    state_key: 'B',
+                },
+            };
+
+            // Request the capability for the wrong state key
+            await loadIframe(['org.matrix.msc2762.receive.state_event:net.example.test#A']);
+
+            emitEvent(new CustomEvent('', { detail: event }));
+
+            await waitFor(() => {
+                expect(transport.reply).toBeCalledWith(event, {
+                    error: { message: expect.any(String) },
+                });
+            });
+
+            expect(driver.readStateEvents).not.toBeCalled()
+        });
+    })
 
     describe('org.matrix.msc3869.read_relations action', () => {
         it('should present as supported api version', () => {