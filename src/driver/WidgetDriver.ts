39f89c4de53649eecdc20ce7357be07536e0f346
Andrew Ferrazzutti <andrewf@element.io>
2024-07-10 11:24:15 -0400

    Update some comments

--- src/driver/WidgetDriver.ts
+++ src/driver/WidgetDriver.ts
@@ -112,12 +112,14 @@ export abstract class WidgetDriver {
     }
 
     /**
+     * @experimental Part of MSC4140 & MSC4157
      * Sends a future into a room. If `roomId` is falsy, the client should send the future
      * into the room the user is currently looking at. The widget API will have already
      * verified that the widget is capable of sending the future's event to that room.
      * @param {string|null} futureGroupId The ID of the group the future belongs to,
-     * or null if it will be put in a new group.
+     * or null if it will be put in a new group. Must not be null if {@link futureTimeout} is null.
      * @param {number|null} futureTimeout The future's timeout, or null for an action future.
+     * Must not be null if {@link futureGroupId} is null.
      * @param {string} eventType The event type of the event to be sent by the future.
      * @param {*} content The content for the event to be sent by the future.
      * @param {string|null} stateKey The state key if the event to be sent by the future is
