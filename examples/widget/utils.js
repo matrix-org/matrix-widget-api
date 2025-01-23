5e9810f2930267d93681b4e91682a26ce8a38a28
Timo <toger5@hotmail.de>
2025-01-23 17:23:51 +0100

    more 4 tab files

--- examples/widget/utils.js
+++ examples/widget/utils.js
@@ -15,17 +15,17 @@
  */
 
 function parseFragment() {
-  const fragmentString = window.location.hash || "?";
-  return new URLSearchParams(fragmentString.substring(Math.max(fragmentString.indexOf("?"), 0)));
+    const fragmentString = window.location.hash || "?";
+    return new URLSearchParams(fragmentString.substring(Math.max(fragmentString.indexOf("?"), 0)));
 }
 
 function assertParam(fragment, name) {
-  const val = fragment.get(name);
-  if (!val) throw new Error(`${name} is not present in URL - cannot load widget`);
-  return val;
+    const val = fragment.get(name);
+    if (!val) throw new Error(`${name} is not present in URL - cannot load widget`);
+    return val;
 }
 
 function handleError(e) {
-  console.error(e);
-  document.getElementById("container").innerText = "There was an error with the widget. See JS console for details.";
+    console.error(e);
+    document.getElementById("container").innerText = "There was an error with the widget. See JS console for details.";
 }
