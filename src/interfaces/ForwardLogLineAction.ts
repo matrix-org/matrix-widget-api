c227453ab63d82176bb567dd6bb205334b3ce75c
Half-Shot <will@half-shot.uk>
2026-01-22 08:58:21 +0000

    update copyright

--- src/interfaces/ForwardLogLineAction.ts
+++ src/interfaces/ForwardLogLineAction.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Nordeck IT + Consulting GmbH
+ * Copyright 2026 The Matrix.org Foundation C.I.C.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
